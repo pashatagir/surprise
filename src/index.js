fb9a2f59bee01848bcbe09f8755fa98a4ed40394
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-01-30 00:47:03 +0200

    added autoplay after click button "Тицяй"

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ const audioEl = document.getElementById('audio');
 btnEl.addEventListener('click', () => {
   containerEl.classList.toggle('is-hidden');
 });
+
+btnEl.addEventListener('click', handlePlayButton, false);
+playAudio();
+
+async function playAudio() {
+  try {
+    await audioEl.play();
+    btnEl.classList.add('playing');
+  } catch (err) {
+    btnEl.classList.remove('playing');
+  }
+}
+
+function handlePlayButton() {
+  if (audioEl.paused) {
+    playAudio();
+  } else {
+    audioEl.pause();
+    audioEl.currentTime = 0;
+    btnEl.classList.remove('playing');
+  }
+}
