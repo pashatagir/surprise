8801fbf1bd3c0fe062c7db284e99023d5e7d45cb
pashatagir <108672605+pashatagir@users.noreply.github.com>
2023-01-29 20:43:48 +0000

    Deploying to gh-pages from @ pashatagir/surprise@0be421c1c8108fdf2af170fc3a3a6ddcc969cbe7 🚀

---
+++ index.697238d8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("btn-surprise"),t=document.getElementById("surprise"),n=document.getElementById("audio");console.log(n.attributes),e.addEventListener("click",(function(e){t.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.697238d8.js.map
