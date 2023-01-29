379c3ac953732931e35112447453cfce5d9e30e7
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-01-29 23:04:34 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ const btnEl = document.getElementById('btn-surprise');
 const containerEl = document.getElementById('surprise');
 const audioEl = document.getElementById('audio');
 
-console.log(audioEl.attributes);
-
-btnEl.addEventListener('click', onClick);
-
-function onClick(params) {
+btnEl.addEventListener('click', () => {
   containerEl.classList.toggle('is-hidden');
-}
+  //   audioEl.setAttribute('autoplay', 'true');
+});
